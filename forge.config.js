e16165d509ebbb66e8ac27269b927ef9f776a0e5
Arshe <2220318422@qq.com>
2026-04-22 16:15:09 +0800

    chore: 添加 Electron 打包配置并更新项目元数据
    - 新增 Electron Forge 配置文件，支持多平台打包和安全性配置
    - 添加应用图标文件作为打包资源
    - 更新 package.json：添加作者信息、应用描述、版本号及打包脚本
    - 调整依赖项顺序并添加 Electron 相关开发依赖
    - 更新 .gitignore 以排除构建输出和本地配置

---
+++ forge.config.js
@@ -0,0 +1,51 @@
+import pluginFuses from '@electron-forge/plugin-fuses';
+import electronFuses from '@electron/fuses';
+
+const { FusesPlugin } = pluginFuses;
+const { FuseV1Options, FuseVersion } = electronFuses;
+
+export default {
+  packagerConfig: {
+    asar: true,
+    icon: './public/logo.ico',
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {
+      },
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['win32', 'darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {
+        options: {
+          icon: './public/logo.ico',
+        },
+      },
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-auto-unpack-natives',
+      config: {},
+    },
+    new FusesPlugin({
+      version: FuseVersion.V1,
+      [FuseV1Options.RunAsNode]: false,
+      [FuseV1Options.EnableCookieEncryption]: true,
+      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+      [FuseV1Options.EnableNodeCliInspectArguments]: false,
+      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      [FuseV1Options.OnlyLoadAppFromAsar]: true,
+    }),
+  ],
+};
